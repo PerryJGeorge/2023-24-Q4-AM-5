749d470072b4cc1e14b5d6f7c342963c8bd6f4c7
MUKILTEO\1714222 <1714222@mukilteo.wednet.edu>
2024-03-27 08:26:32 -0700

    Animation blend trees

--- Q4Project/Assets/Ian/PlayerCtrl.cs
+++ Q4Project/Assets/Ian/PlayerCtrl.cs
@@ -9,14 +9,20 @@ public class PlayerCtrl : MonoBehaviour
     public float movSpeed;
     float speedX, speedY;
     Rigidbody2D rb; 
-
-
+    private Animator animator;
+    p
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Ainmator>();
+    }
 
+    public void OnMove(InputValue value)
+    {
+        animator.SetFloat("XInput", moveInput.x);
+        animator.SetFloat("XInput", moveInput.x);
     }
 
     // Update is called once per frame
