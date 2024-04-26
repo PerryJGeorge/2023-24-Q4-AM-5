54c714e62d586a33b6a730859f9b6fe243a6da2e
George <1717784@mukilteo.wednet.edu>
2024-04-26 09:04:17 -0700

    finished the block moving scripts

--- Q4Project/Assets/Perry G/Block_Push.cs
+++ Q4Project/Assets/Perry G/Block_Push.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Block_Push : MonoBehaviour
 {
 
+    
+    public GameObject player;
+    public GameObject me;
+    public void MoveMe()
+    {
+        if (push_button.dir==1)
+        {
+            me.transform.Translate(new Vector3(1,0,0));
+        }
+        if (push_button.dir == -1)
+        {
+            me.transform.Translate(new Vector3(-1, 0, 0));
+        }
+        if (push_button.dir == 2)
+        {
+            me.transform.Translate(new Vector3(0, 1, 0));
+        }
+        if (push_button.dir == -2)
+        {
+            me.transform.Translate(new Vector3(0, -1, 0));
+        }
+    }
 }
