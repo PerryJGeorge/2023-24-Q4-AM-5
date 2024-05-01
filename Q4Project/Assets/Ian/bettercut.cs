34bf458a1223c3f9e58d52a08941c45e2ade5dc5
MUKILTEO\1714222 <1714222@mukilteo.wednet.edu>
2024-05-01 10:00:38 -0700

    the rocker

--- Q4Project/Assets/Ian/bettercut.cs
+++ Q4Project/Assets/Ian/bettercut.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class bettercut : MonoBehaviour
 {
     public DialogueTriggerScript Dcut;
+    public PlayerCtrl Pctrl;
     // Start is called before the first frame update
     void Start()
     {
+        Pctrl.movSpeed = 0;
         Dcut.TriggerDialogue();
     }
 }
