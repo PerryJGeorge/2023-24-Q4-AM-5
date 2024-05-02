6649a2d2efdd4149daffd0d4f8d3e8ad5192b8b8
MatthewM62 <1717212@mukilteo.wednet.edu>
2024-05-02 09:56:07 -0700

    Added random encounters

---
+++ Q4Project/Assets/RandomEncounterTown.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEncounterTown : MonoBehaviour
+{
+    public flick loadbattlescene;
+
+    void Start()
+    {
+        while (true)
+        {
+            StartCoroutine(CheckForScene());
+        }
+    }
+
+    IEnumerator CheckForScene()
+    {
+        yield return new WaitForSeconds(1f);
+        if (Random.Range(1, 101) <= 10)
+        {
+            loadbattlescene.UniqueExit();
+        }
+    }
+}
